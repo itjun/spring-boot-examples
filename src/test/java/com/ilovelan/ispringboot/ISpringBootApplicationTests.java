fef86f9636511cdf6b8c49776d550dc065927cee
黄荣君 <l1091462907@gmail.com>
2018-04-09 21:15:05 +0800

    init repository

---
+++ src/test/java/com/ilovelan/ispringboot/ISpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ilovelan.ispringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ISpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
