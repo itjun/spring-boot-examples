fabefe94a79bb96ccb209e3221ac7abdec433138
黄荣君 <l1091462907@gmail.com>
2018-04-25 23:43:39 +0800

    增加热部署配置

--- spring-boot-hello/src/main/java/com/ilovelan/hello/controller/HelloController.java
+++ spring-boot-hello/src/main/java/com/ilovelan/hello/controller/HelloController.java
@@ -1,15 +1,13 @@
 package com.ilovelan.hello.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
-        return "Hello, Spring Boot 2.0!";
+        return "Hello, Spring Boot 2.0 !";
     }
 }
