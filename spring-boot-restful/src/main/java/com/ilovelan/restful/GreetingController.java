b4923026d52e08a10e0e14bc32a876e7bfb01c54
黄荣君 <l1091462907@gmail.com>
2018-06-11 22:41:45 +0800

    增加日志输出

--- spring-boot-restful/src/main/java/com/ilovelan/restful/GreetingController.java
+++ spring-boot-restful/src/main/java/com/ilovelan/restful/GreetingController.java
@@ -1,5 +1,7 @@
 package com.ilovelan.restful;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,14 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        log.info("name = {}", name);
         return new Greeting(counter.getAndIncrement(),
                 String.format(template, name));
     }
