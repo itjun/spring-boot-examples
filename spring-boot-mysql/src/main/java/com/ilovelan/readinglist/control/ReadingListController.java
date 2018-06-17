ddc932d8e1516231a965d3b72d4c0aba02b85151
黄荣君 <l1091462907@gmail.com>
2018-06-17 15:06:41 +0800

    增加mysql的使用范例

---
+++ spring-boot-mysql/src/main/java/com/ilovelan/readinglist/control/ReadingListController.java
@@ -0,0 +1,45 @@
+package com.ilovelan.readinglist.control;
+
+import com.ilovelan.readinglist.entity.Book;
+import com.ilovelan.readinglist.entity.ReadingListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/readingList")
+public class ReadingListController {
+
+    private ReadingListRepository readingListRepository;
+    private static final String READER = "jake";
+
+    @Autowired
+    public ReadingListController(ReadingListRepository readingListRepository) {
+        this.readingListRepository = readingListRepository;
+    }
+
+    @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
+    public String readerBooks(@PathVariable("reader") String reader, Model model) {
+        if (reader == null || "".equals(reader)) {
+            reader = READER;
+        }
+        List<Book> readingList = readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
+        }
+        return "readingList";
+    }
+
+    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
+    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+        book.setReader(reader);
+        readingListRepository.save(book);
+        return "redirect:/readingList/{reader}";
+    }
+
+}
