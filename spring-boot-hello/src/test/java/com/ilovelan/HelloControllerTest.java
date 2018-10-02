ced03d73e370e1887d70c94f7c550fcdb83894f8
黄荣君 <l1091462907@gmail.com>
2018-10-03 00:27:01 +0800

    增加热部署和测试用例

---
+++ spring-boot-hello/src/test/java/com/ilovelan/HelloControllerTest.java
@@ -0,0 +1,34 @@
+package com.ilovelan;
+
+import com.ilovelan.hello.HelloController;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+public class HelloControllerTest {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/hello?name=Spring")
+                .accept(MediaType.APPLICATION_JSON_UTF8)).andDo(print())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Spring")));
+    }
+
+
+}
