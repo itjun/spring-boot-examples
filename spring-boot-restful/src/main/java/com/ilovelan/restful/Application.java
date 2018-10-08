57a5bc7ac3b9042e4d0ac02a60b5021d830b4ee0
黄荣君 <l1091462907@gmail.com>
2018-10-08 11:34:23 +0800

    update restful

--- spring-boot-restful/src/main/java/com/ilovelan/restful/Application.java
+++ spring-boot-restful/src/main/java/com/ilovelan/restful/Application.java
@@ -2,11 +2,20 @@ package com.ilovelan.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+    @RequestMapping("/")
+    public String start(){
+        return "Spring Boot 2.0 正常启动";
+    }
+
 }
