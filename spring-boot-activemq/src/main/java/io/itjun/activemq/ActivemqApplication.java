46eb548821147cb4187a987cd8c4ad791ae78b45
黄荣君 <l1091462907@gmail.com>
2018-11-17 17:44:13 +0800

    format code

--- spring-boot-activemq/src/main/java/io/itjun/activemq/ActivemqApplication.java
+++ spring-boot-activemq/src/main/java/io/itjun/activemq/ActivemqApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ActivemqApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ActivemqApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActivemqApplication.class, args);
+    }
 }
