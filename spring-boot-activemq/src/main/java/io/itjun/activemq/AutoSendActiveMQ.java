ad1278ee2b44969e59f82f3d1a48c3085ea51eb8
黄荣君 <l1091462907@gmail.com>
2018-11-19 10:13:59 +0800

    add rabbitmq

---
+++ spring-boot-activemq/src/main/java/io/itjun/activemq/AutoSendActiveMQ.java
@@ -0,0 +1,32 @@
+package io.itjun.activemq;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+import io.itjun.activemq.client.ActiveMQClient;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class AutoSendActiveMQ {
+
+    @Autowired
+    ActiveMQClient client;
+
+    @PostConstruct
+    @Scheduled(fixedDelay = 3 * 1000)
+    public void init() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        for (int i = 0; i < 10; i++) {
+            client.send("----itjun-----");
+        }
+        stopWatch.stop();
+        log.info("发送消息耗时 {}", stopWatch.getTotalTimeMillis());
+    }
+
+}
