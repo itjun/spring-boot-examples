b98cd7c87e38717e186116a1adbed7f41bfa3bd5
黄荣君 <l1091462907@gmail.com>
2018-11-23 10:01:40 +0800

    add spring-boot-jsp

---
+++ sping-boot-jsp/src/main/java/io/itjun/jsp/JspApplication.java
@@ -0,0 +1,12 @@
+package io.itjun.jsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JspApplication.class, args);
+    }
+}
