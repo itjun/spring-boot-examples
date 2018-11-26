a59254c250d852be8350854bc4d5c00355f959ac
黄荣君 <l1091462907@gmail.com>
2018-11-26 11:17:00 +0800

    format

--- spring-boot-actuator/src/main/java/io/itjun/actuator/ActuatorApplication.java
+++ spring-boot-actuator/src/main/java/io/itjun/actuator/ActuatorApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootActuatorApplication {
+public class ActuatorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootActuatorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
 }
