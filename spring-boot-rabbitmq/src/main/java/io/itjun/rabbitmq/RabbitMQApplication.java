308546db16ff63ef41a4353a7d6b1ef96ac235f0
黄荣君 <l1091462907@gmail.com>
2018-12-20 11:41:14 +0800

    Update RabbitMQApplication.java

--- spring-boot-rabbitmq/src/main/java/io/itjun/rabbitmq/RabbitMQApplication.java
+++ spring-boot-rabbitmq/src/main/java/io/itjun/rabbitmq/RabbitMQApplication.java
@@ -1,13 +1,12 @@
 package io.itjun.rabbitmq;
 
-import io.itjun.rabbitmq.config.RabbitMQConfig;
 import org.springframework.amqp.core.Queue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import io.itjun.rabbitmq.config.RabbitMQConfig;
 
 @EnableScheduling
 @SpringBootApplication
